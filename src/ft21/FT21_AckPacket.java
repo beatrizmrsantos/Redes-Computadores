5566477bfa3425770c39c3c9fb338766f98cd045
MarciaCampanha <m.campanha@campus.fct.unl.pt>
2021-12-13 19:44:20 +0000

    correção ack

--- src/ft21/FT21_AckPacket.java
+++ src/ft21/FT21_AckPacket.java
@@ -3,21 +3,20 @@ package ft21;
 public class FT21_AckPacket extends FT21Packet {
 	public final int cSeqN;
 	public final boolean outsideWindow;
-	public final int optional_data_field;
+	public final int optional_data_field ;
 	
-	FT21_AckPacket(byte[] bytes, int optional_data_field) {
+	FT21_AckPacket(byte[] bytes) {
 		super( bytes );
 		int seqN = super.getInt();
 		this.cSeqN = Math.abs( seqN );
 		this.outsideWindow = seqN < 0;
-		super.putByte(1 * Integer.BYTES);
-		this.optional_data_field = optional_data_field;
+		this.optional_data_field = super.getInt();
 		
 		// decode optional fields here...
 	}
 
 	public String toString() {
-		return String.format("ACK<%d %d>", cSeqN, optional_data_field);
+		return String.format("ACK<%d >", cSeqN);
 	}
 	
 }

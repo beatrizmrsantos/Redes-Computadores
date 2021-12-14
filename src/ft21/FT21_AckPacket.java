f2bd0639ce426b89d1655e5e3f9e65ee7d40383f
Beatriz <bmr.santos@campus.fct.unl.pt>
2021-12-14 19:14:56 +0000

    mal

--- src/ft21/FT21_AckPacket.java
+++ src/ft21/FT21_AckPacket.java
@@ -3,6 +3,8 @@ package ft21;
 public class FT21_AckPacket extends FT21Packet {
 	public final int cSeqN;
 	public final boolean outsideWindow;
+	public final byte b;
+	public final byte[] bb;
 	public final int optional_data_field ;
 	
 	FT21_AckPacket(byte[] bytes) {
@@ -10,8 +12,11 @@ public class FT21_AckPacket extends FT21Packet {
 		int seqN = super.getInt();
 		this.cSeqN = Math.abs( seqN );
 		this.outsideWindow = seqN < 0;
+		b = super.getByte();
+		bb = super.getBytes(b);
 		this.optional_data_field = super.getInt();
-		
+
+
 		// decode optional fields here...
 	}
 
